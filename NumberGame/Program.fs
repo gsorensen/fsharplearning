43605ab1b1a7c3da4c73414f2ff323da2e4ef6ca
Glen H. M. Sørensen <gsorensen1201@gmail.com>
2021-11-29 20:10:59 +0100

    Second iteration

--- NumberGame/Program.fs
+++ NumberGame/Program.fs
@@ -1,52 +1,62 @@
-open System
+type GuessResult = | BelowLowerBound | BelowAnswer | Correct | AboveUpperBound | AboveAnswer 
 
 let printToConsole message = printfn "%s "message
     
 let parseInt (str: string) = 
-    match Int32.TryParse str with 
+    match System.Int32.TryParse str with 
     | (true, v) -> Some v 
     | (false, _) -> None
 
-let parseValidInputArguments consoleInput =
-    consoleInput 
-    |> Array.toList
-    |> List.map (fun v -> parseInt v) 
-    |> List.filter(fun v -> v.IsSome)
-    |> List.map(fun v -> v.Value)
+let parseValidInputArguments (input: string[]) =
+    input 
+    |> Array.toList 
+    |> List.map (fun v -> parseInt v) |> List.filter(fun v -> v.IsSome) |> List.map(fun v -> v.Value) 
     |> List.sort
 
 let getGameBounds consoleInput defaultLower defaultUpper =
     let parsedConsoleInput = parseValidInputArguments consoleInput        
     match parsedConsoleInput.Length with 
-    | 1 -> (defaultLower, parsedConsoleInput.[0]) 
+    | 1 when parsedConsoleInput.[0] > defaultLower -> (defaultLower, parsedConsoleInput.[0]) 
     | 2 -> (parsedConsoleInput.[0], parsedConsoleInput.[1]) 
     | _ -> (defaultLower, defaultUpper) 
 
-let compare guess answer lowerBound upperBound =
-    match guess with 
-    | guess when guess < lowerBound -> (false, $"{guess} is below {lowerBound}!")
-    | guess when guess > upperBound -> (false, $"{guess} is above {upperBound}!")
-    | guess when guess < answer -> (false, $"{guess} is too low!")
-    | guess when guess > answer -> (false, $"{guess} is too high!") 
-    | guess -> (true, $"{guess} is the correct answer!")
+let checkGuess (guess: int) (answer: int) (bounds: int*int) : GuessResult =
+    match (guess, answer) with 
+    | (guess, answer) when guess > answer ->
+        match (guess, snd bounds) with 
+        | (guess, upperBound) when guess > upperBound -> AboveUpperBound
+        | _ -> AboveAnswer
+    | (guess, answer) when guess < answer ->
+        match (guess, fst bounds) with 
+        | (guess, lowerBound) when guess < lowerBound -> BelowLowerBound 
+        | _ -> BelowAnswer
+    | _ -> Correct
 
-let rec getNumberGuess correctAnswer lowerBound upperBound=
-    printf "Your guess: "
-    match parseInt (Console.ReadLine()) with 
+let resultString (result: GuessResult) = 
+    match result with 
+    | Correct -> "That is the correct answer!"
+    | AboveAnswer -> "That number is too high!"
+    | AboveUpperBound -> "That number is above the upper bound!"
+    | BelowAnswer -> "That number is too low!"
+    | BelowLowerBound -> "That number is below the lower bound!"
+
+let displayResult (result: GuessResult) = resultString result |> printToConsole
+
+let rec getNumberGuess (correctAnswer: int) (bounds: int*int) =
+    match parseInt (System.Console.ReadLine()) with 
     | None -> 
         printToConsole "You need to guess integers!"
-        getNumberGuess correctAnswer lowerBound upperBound
+        getNumberGuess correctAnswer bounds
     | Some guess  -> 
-        match compare guess correctAnswer lowerBound upperBound with 
-        | (true, victoryMsg) -> victoryMsg
-        | (false, errorMsg) -> 
-            printToConsole errorMsg
-            getNumberGuess correctAnswer lowerBound upperBound
+        let guessResult = checkGuess guess correctAnswer bounds
+        displayResult guessResult
+        match guessResult with 
+        | Correct -> 0
+        | _ -> getNumberGuess correctAnswer bounds
 
 [<EntryPoint>]
 let main argv =
-    let lowerBound, upperBound = getGameBounds argv 1 50
-    let correctAnswer = ((Random()).Next(lowerBound, upperBound)) 
-    printToConsole $"I am thinking of a number between {lowerBound} and {upperBound}... guess it!"  
-    printToConsole (getNumberGuess correctAnswer lowerBound upperBound)
-    0 
+    let bounds = getGameBounds argv 1 50
+    let correctAnswer = ((System.Random()).Next(fst bounds, snd bounds)) 
+    printToConsole $"I am thinking of a number between {fst bounds} and {snd bounds}... guess it!"  
+    getNumberGuess correctAnswer bounds
